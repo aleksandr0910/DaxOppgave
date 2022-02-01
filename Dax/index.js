ee3dd96843db53559eb22f22da584f0b7123a734
aleksandr0910 <alringstad@outlook.com>
2022-02-01 10:57:16 +0100

    nye forandringer

--- Dax/index.js
+++ Dax/index.js
@@ -1,8 +1,14 @@
 
     let navn = document.getElementById("søkefelt");
     let takkefelt = document.getElementById("takkefelt");
+    let navner = localStorage.getItem("names") ? JSON.parse(localStorage.getItem("names")) : 
+    localStorage.setItem("names", "[]") || [];
 function navninput(){
-    takkefelt.innerHTML = "takk for at du besøkte denne siden " + navn.value ;
+    takkefelt.innerHTML = "Takk for at du besøkte denne siden " + navn.value ;
+    
+    navner.push(navn.value)
+    localStorage.setItem("names", JSON.stringify(navner))
+
     localStorage.setItem("name", navn.value)
     
 }
@@ -11,21 +17,26 @@ function lykketall(){
     let lykke = Math.floor(Math.random()*300)
     let tallet = document.getElementById("tallET") 
     tallet.innerHTML = "ditt lykketall er " + lykke;
+  
 
 }
-lykketall();
 
 function lokallagring(){
     let lokal = document.getElementById("test");
-    lokal.innerHTML = localStorage.getItem("name", navn.value); 
+    lokal.innerHTML = localStorage.getItem("name", navn.value);
     
 }
 lokallagring();
 
-function babymaker(){
-   
-    const para = document.createElement("p");
-    para.innerHTML = navn.value;
-    document.querySelector("fieldset").appendChild(para);
-    
+function pmaker(){
+    document.querySelector("#tidligere-besøkende").innerHTML = navner.join("<br>");
+    navn.value="";
 }
+pmaker();
+
+
+setTimeout(() => {
+    lykketall()
+    setInterval(() => { 
+    }, 60 * 1000);
+  }, (60 - new Date().getSeconds()) * 1000);
